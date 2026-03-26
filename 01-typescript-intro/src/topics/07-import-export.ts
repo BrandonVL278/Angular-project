43d07dfe00a1e5266d98b339729cd93bda25239f
cristian <158619698+BrandonVL278@users.noreply.github.com>
2026-03-25 18:50:06 -0600

    third commit

---
+++ 01-typescript-intro/src/topics/07-import-export.ts
@@ -0,0 +1,31 @@
+
+import { taxCalculation }from './06-function-destructuring'
+import type { Product } from './06-function-destructuring'
+
+
+const shoppingCart: Product[] = [
+    {
+        description: 'Nokia',
+        price: 100
+    },
+    {
+        description: 'iPad',
+        price: 150
+    }
+];
+
+const [ total, tax] = taxCalculation({
+    products: shoppingCart,
+    tax: .15
+});
+
+
+
+console.log('Total', total);
+console.log('Tax', tax)
+
+
+
+
+
+
